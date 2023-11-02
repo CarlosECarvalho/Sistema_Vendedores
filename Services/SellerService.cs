89ddd6a73387d84ffa747e19ed49693fa0e244b1
Carlos  Eduardo Ferreira Carvalho <carlos_ferreira@id.uff.br>
2023-11-02 15:48:20 -0300

    SallesService e FindAll

---
+++ Services/SellerService.cs
@@ -0,0 +1,20 @@
+﻿using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMVCContext _context;
+
+        public SellerService(SalesWebMVCContext context)//injeto dependencia com o MVCContext para acessar os dados
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
